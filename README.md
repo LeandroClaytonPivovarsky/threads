6bc6e9242f85feb32369d154c3d4c22250d61aad
LeandroClaytonPivovarsky <leandro123clayton@gmail.com>
2024-10-09 20:33:00 -0300

    Trying to make threads with synchronized method

---
+++ README.md
@@ -0,0 +1,3 @@
+"Projeto" em java com intuito de brincar com o uso de Threads
+
+Consiste em uma HUB de Threads que ficam rodando infinitamente até que algum input faça parar.
--- src/main/java/com/threads/App.java
+++ src/main/java/com/threads/App.java
@@ -1,13 +1,81 @@
 package com.threads;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static ThreadHub threadHub;
     public static void main( String[] args )
     {
+        Scanner reader =  new Scanner(System.in);
         
+        boolean continuar = true;
+        threadHub = new ThreadHub();
+        threadHub.start();
+        while(continuar){
+            
+            System.out.println("Type something ant press enter to get one of this actions");
+            reader.next();
+
+            threadHub.allThreadsInterruptor();
+            
+            System.out.println("Your answer: \n");
+            
+            String input = reader.next();
+            
+
+            if (input.equalsIgnoreCase("sair")) {
+                continuar = false;
+            } else if (input.isEmpty() || input.isBlank()) {
+                System.out.println("Nenhuma resposta registrada");
+            }{
+            choices(input, reader);
+        }
+            //threadHub.allThreadsStarter();
+            
+            threadHub.printInterface();
+
+        }
+
+        
+    }
+
+
+    private static void choices(String input, Scanner reader){
+
+        input = input.toUpperCase();
+        switch (input) {
+            case "KILL ALL":
+                threadHub.kill();
+                break;
+        
+            case "ADD":
+                threadHub.addThread();
+                System.out.println("Thread adicionada com sucesso!");
+                break;
+            case "KILL ONE THREAD":
+                boolean escolha = false;
+
+
+                if (!threadHub.listAllThreads()) {
+                    System.out.println("Nenhuma thread iniciada!");
+                    return;
+                }
+                System.out.println("Which one do you want to kill?(only numbers)");
+                while (!escolha) { 
+                    int id = reader.nextInt();
+                    if (threadHub.killOneThread(id)) {
+                        System.out.println("Thread with ID " + id + " deleted!");
+                        escolha = true;
+                    } else{
+                        System.out.println("The thread with this ID does not exist");
+                    }
+                }
+
+        }
     }
 }
